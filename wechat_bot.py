c074277584b486175f20cade9016fbd0f04b944e
Jackson Ming Hu <huming2207@gmail.com>
2017-04-14 14:24:04 +1000

    Try another framework

--- wechat_bot.py
+++ wechat_bot.py
@@ -1,19 +1,70 @@
-import itchatmp
+import os
+
+from flask import Flask, request, abort, render_template
+from wechatpy import parse_message, create_reply
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import (
+    InvalidSignatureException,
+    InvalidAppIdException,
+)
+
 import wechat_bot_account
-import naips_bot, naips_account
 
-itchatmp.update_config(itchatmp.WechatConfig(
-    token = wechat_bot_account.wechat_token,
-    appId = wechat_bot_account.wechat_mp_appid,
-    appSecret = wechat_bot_account.wechat_mp_appkey))
+# set token or get from environments
+TOKEN = os.getenv('WECHAT_TOKEN', wechat_bot_account.wechat_token)
+AES_KEY = os.getenv('WECHAT_AES_KEY', wechat_bot_account.wechat_mp_appkey)
+APPID = os.getenv('WECHAT_APPID', wechat_bot_account.wechat_mp_appid)
 
-
-@itchatmp.msg_register(itchatmp.content.TEXT)
-def text_reply(msg):
-    msg_str = msg['Content']
-
-    if "ymml atis" or "YMML ATIS" or "ymml" or "YMML" in msg_str:
-        return naips_bot.get_met_briefing("YMML", naips_bot.napis_user_login(naips_bot.get_initial_cookie(), naips_account.naips_username, naips_account.naips_password))
+app = Flask(__name__)
 
 
-itchatmp.run(port=8081)
+
+@app.route('/', methods=['GET', 'POST'])
+def wechat():
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+    encrypt_type = request.args.get('encrypt_type', 'raw')
+    msg_signature = request.args.get('msg_signature', '')
+    try:
+        check_signature(TOKEN, signature, timestamp, nonce)
+    except InvalidSignatureException:
+        abort(403)
+    if request.method == 'GET':
+        echo_str = request.args.get('echostr', '')
+        return echo_str
+
+    # POST request
+    if encrypt_type == 'raw':
+        # plaintext mode
+        msg = parse_message(request.data)
+        if msg.type == 'text':
+            reply = create_reply(msg.content, msg)
+        else:
+            reply = create_reply('Sorry, can not handle this for now', msg)
+        return reply.render()
+    else:
+        # encryption mode
+        from wechatpy.crypto import WeChatCrypto
+
+        crypto = WeChatCrypto(TOKEN, AES_KEY, APPID)
+        try:
+            msg = crypto.decrypt_message(
+                request.data,
+                msg_signature,
+                timestamp,
+                nonce
+            )
+        except (InvalidSignatureException, InvalidAppIdException):
+            abort(403)
+        else:
+            msg = parse_message(msg)
+            if msg.type == 'text':
+                reply = create_reply(msg.content, msg)
+            else:
+                reply = create_reply('Sorry, can not handle this for now', msg)
+            return crypto.encrypt_message(reply.render(), nonce, timestamp)
+
+
+if __name__ == '__main__':
+    app.run(80, debug=True)
